fbc4e8dbbbcf4ff086c2e5e685c8155d57cf04d8
AbdiCytus <abdiprayuda203@gmail.com>
2026-03-21 14:09:15 +0800

    Project Fix - Fix brand logo (2)

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,4 +2,13 @@ const serverless = require("serverless-http");
 const app = require("../../src/app");
 
 // Membungkus aplikasi Express agar bisa dijalankan oleh Netlify
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app, {
+  binary: [
+    "image/png",
+    "image/jpeg",
+    "image/gif",
+    "image/webp",
+    "image/svg+xml",
+    "image/*",
+  ],
+});
